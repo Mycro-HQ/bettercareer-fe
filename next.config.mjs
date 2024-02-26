07037629e1549261077867418d72134657120d1a
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-02-26 05:29:16 +0100

    chore(next.config.mjs): enable SWC minification and add webpack rule for SVG files feat(package.json): add @radix-ui/themes and zustand dependencies feat(package.json): add lint-staged and husky for pre-commit hooks feat(package.json): update devDependencies including @svgr/webpack, eslint, husky, lint-staged, prettier, and sass
    chore(tsconfig.json): enable strictNullChecks
    feat(tsconfig.json): add paths for @lib/* and @labs/* to resolve to corresponding directories

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	swcMinify: true,
+
+	webpack: (config, { isServer }) => {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: [
+				{
+					loader: '@svgr/webpack',
+					options: {
+						svgoConfig: {
+							plugins: [
+								{ name: 'removeViewBox', active: false },
+								{
+									name: 'preset-default',
+									params: {
+										overrides: {
+											removeViewBox: false,
+											convertShapeToPath: false,
+											inlineStyles: false,
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			],
+		});
+
+		return config;
+	},
 };
 
 export default nextConfig;
