b4892b2d273b82ea31dcb93f276ccbfc70d61822
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-03-07 01:21:17 +0000

    style-creator.ts: fix variable name from 'augumentedValue' to 'augmentValue' base-query.ts: add base query function using axios for react-query integration config.ts: add API_URL and GOOGLE_AUTH_ID constants from environment variables query-client.ts: create query client with default options for react-query usable-query.ts: add utility functions and types for handling queries and mutations createSmartApi: create smart API from a set of endpoints for query and mutation smartListen: create smart Listener API from a set of endpoints for query and mutation useCustomQuery: custom hook to handle onSuccess and onError logic in queries

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,3 @@
 export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const GOOGLE_AUTH_ID = process.env.NEXT_PUBLIC_GOOGLE_AUTH_ID!;
