e5a49274294ffe158782e9c60e6cc16168d4a3c6
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-03-13 10:52:24 +0100

    Refactor notification component and add notification header

--- src/pages/dashboard/notification.tsx
+++ src/pages/dashboard/notification.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
 
-import Notifications from '@/components/Notifications';
+import BellIcon from '@labs/icons/dashboard/bell.svg';
+import { Flex, Heading, Text } from '@labs/components';
+
+import Notifications, {
+	NotificationListItem,
+} from '@/components/Notifications';
 import { DashboardLayout } from '@/features/dashboard/layout';
+
+function NotificationHeader() {
+	return (
+		<Flex.Column gap={6}>
+			<Flex alignItems="center" gap={12}>
+				<Heading.h3 weight={400} animate="slide">
+					Notifications
+				</Heading.h3>
+				<BellIcon />
+			</Flex>
+
+			<Text color="var(--text-gray)" animate="fade" className="mb-[40px]">
+				Lets catch you up on what you missed.
+			</Text>
+		</Flex.Column>
+	);
+}
 
 export default function Notification() {
 	return (
 		<DashboardLayout backdropThreshold="md" title="Notifications">
-			<Notifications />
+			<NotificationHeader />
+			<Notifications>
+				<NotificationListItem
+					title="Top Job Match: Product Designer at Dribbble"
+					description="This opportunity closely aligns with your skills and preferences. Act fast, applications close soon!"
+					time="30 mins"
+				/>
+				<NotificationListItem
+					title="Top Job Match: Product Designer at Dribbble"
+					description="This opportunity closely aligns with your skills and preferences. Act fast, applications close soon!"
+					time="30 mins"
+				/>
+				<NotificationListItem
+					title="Top Job Match: Product Designer at Dribbble"
+					description="This opportunity closely aligns with your skills and preferences. Act fast, applications close soon!"
+					time="30 mins"
+				/>
+				<NotificationListItem
+					type="projectSetup"
+					title="Your profile is 75% complete"
+					description="Adding a few details can significantly increase your visibility to recruiters. Finish setting up your profile today."
+					time="30 mins"
+				/>
+			</Notifications>
 		</DashboardLayout>
 	);
 }
