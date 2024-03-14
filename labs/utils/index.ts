5ab98f53e1757de4bae238465f993be520c5d72b
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-03-14 22:30:30 +0000

    Add exceptions module to utils

--- labs/utils/index.ts
+++ labs/utils/index.ts
@@ -3,3 +3,4 @@ export * from './hooks/useStyles';
 export * from './hooks/useIsomorphicLayoutEffect';
 export * from './hooks/useDyanmicStyle';
 export * from './misc';
+export * from './exceptions';
