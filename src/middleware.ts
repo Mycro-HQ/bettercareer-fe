3d55ab685cf78b96eb31868d7a3f6448eb8ae841
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-03-18 15:00:17 +0100

    Upfix edge function

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,16 +10,17 @@ export function middleware(req: NextRequest) {
 	const hasToken = req.cookies.has('bc_token');
 	const url = req.nextUrl.clone(); // Clone the URL to modify it
 
-	// Redirect users to the home page if the app is in testing mode and they are not on the home page
-	if (process.env.NEXT_PUBLIC_TESTING === 'true' && url.pathname !== '/') {
-		return NextResponse.redirect('/');
-	}
-
 	const redirectResponse = (url: string | NextURL) => {
 		const response = NextResponse.redirect(url);
 		response.headers.set('x-middleware-cache', 'no-cache'); // ! FIX: Disable caching
 		return response;
 	};
+
+	// Redirect users to the home page if the app is in testing mode and they are not on the home page
+	if (process.env.NEXT_PUBLIC_TESTING === 'true' && url.pathname !== '/') {
+		url.pathname = '/';
+		return redirectResponse(url);
+	}
 
 	// Redirects for users with a token trying to access login, register, or forgot-password
 	if (hasToken && ['/login', '/register'].includes(url.pathname)) {
