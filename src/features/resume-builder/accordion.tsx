099934e503e2ed78f5cda69aa5eaa5777d076c97
Philzace1 <unknownumeh@gmail.com>
2024-03-20 23:45:48 +0100

    draft pr

---
+++ src/features/resume-builder/accordion.tsx
@@ -0,0 +1,128 @@
+import React, { useState, ReactNode } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Flex } from '@labs/components';
+import { Droppable, Draggable } from 'react-beautiful-dnd'; // Import Draggable and Droppable
+import styles from './resume.module.scss';
+import DragAndDropIcon from '@labs/icons/misc/DragAndDropIcon.svg';
+
+interface AccordionProps {
+	children: ReactNode;
+}
+
+interface PanelProps {
+	children: ReactNode;
+	title: string;
+	index: number; // Add index prop to PanelProps
+}
+
+interface TitleProps {
+	children: ReactNode;
+}
+
+interface ContentProps {
+	children: ReactNode;
+}
+
+const Accordion: React.FC<AccordionProps> & {
+	Panel: React.FC<PanelProps>;
+	Title: React.FC<TitleProps>;
+	Content: React.FC<ContentProps>;
+} = ({ children }) => {
+	const [selected, setSelected] = useState<number | null>(null);
+
+	const togglePanel = (index: number) => {
+		setSelected(selected === index ? null : index);
+	};
+
+	return (
+		<Droppable droppableId="accordion">
+			{(provided) => (
+				<div
+					className="accordion"
+					ref={provided.innerRef}
+					{...provided.droppableProps}
+				>
+					{React.Children.map(children, (child, index) => {
+						if (!React.isValidElement(child)) {
+							return null;
+						}
+
+						return (
+							<Draggable
+								key={index}
+								draggableId={`panel-${index}`}
+								index={index}
+							>
+								{(provided) => (
+									<div
+										ref={provided.innerRef}
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+									>
+										<div className={styles.AccordionPanel}>
+											<Flex.Row
+												onClick={() => togglePanel(index)}
+												className="text-neutral-600 font-semibold"
+												alignItems="center"
+											>
+												<DragAndDropIcon />
+												<div className="ml-2">
+													{child.props.title || 'Untitled'}
+												</div>
+											</Flex.Row>
+											<AnimatePresence>
+												{selected === index && (
+													<motion.div
+														key="content"
+														initial="collapsed"
+														animate="open"
+														exit="collapsed"
+														variants={{
+															open: {
+																opacity: 1,
+																height: 'auto',
+																overflow: 'hidden',
+															},
+															collapsed: {
+																opacity: 0,
+																height: 0,
+																overflow: 'hidden',
+															},
+														}}
+														transition={{ duration: 0.3 }}
+														className={styles.AccordionContent}
+													>
+														{child.props.children}
+													</motion.div>
+												)}
+											</AnimatePresence>
+										</div>
+									</div>
+								)}
+							</Draggable>
+						);
+					})}
+					{provided.placeholder}
+				</div>
+			)}
+		</Droppable>
+	);
+};
+
+const Panel: React.FC<PanelProps> = ({ children }) => {
+	return <>{children}</>;
+};
+
+const Title: React.FC<TitleProps> = ({ children }) => {
+	return <div className="accordion-title">{children}</div>;
+};
+
+const Content: React.FC<ContentProps> = ({ children }) => {
+	return <div className="accordion-content">{children}</div>;
+};
+
+Accordion.Panel = Panel;
+Accordion.Title = Title;
+Accordion.Content = Content;
+
+export default Accordion;
---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useUserStore } from '@/store/z-store/user';
+import { OnboardingLayout } from '@/features/onboarding';
+import ResumeBody from '@/features/resume-builder/resume-body';
+import { CallToAction, Flex, Heading, Text } from '@labs/components';
+import StarIcon from '@labs/icons/misc/star.svg';
+import { ResumeBuilderHeader } from '@/features/resume-builder/header';
+import styles from '@/features/resume-builder/resume.module.scss';
+
+export const Dashboard = () => {
+	const { profile } = useUserStore();
+
+	return (
+		<>
+			<ResumeBuilderHeader />
+			<OnboardingLayout
+				header={false}
+				title={'Resume Builder'}
+				center={false}
+				adjacentContent={<>Hi There</>}
+			>
+				<AnimatePresence>
+					<div>
+						<motion.div
+							initial={{ opacity: 0, y: 15 }}
+							animate={{ opacity: 1, y: 0 }}
+							exit={{ opacity: 0, y: 15 }}
+						>
+							<Flex.Row justifyContent="space-between">
+								<Flex.Row alignItems="flex-start" justifyContent="flex-start">
+									<StarIcon />
+									<Flex.Column gap={14} className="ml-2">
+										<Heading.h4>Build Resume</Heading.h4>
+										<Text as="p" className="text-neutral-600">
+											Edit Section below and see your result immediately
+										</Text>
+									</Flex.Column>
+								</Flex.Row>
+
+								<CallToAction.button
+									className={styles.ResumeButton}
+									outline
+									variant="primary"
+									style={{
+										background: 'red',
+									}}
+								>
+									Tailor to Job
+								</CallToAction.button>
+							</Flex.Row>
+						</motion.div>
+					</div>
+				</AnimatePresence>
+				<ResumeBody />
+
+				<Flex.Row justifyContent="flex-end">
+					<CallToAction.button outline className="mt-10">
+						Resources and support
+					</CallToAction.button>
+				</Flex.Row>
+			</OnboardingLayout>
+		</>
+	);
+};
+
+export default Dashboard;
