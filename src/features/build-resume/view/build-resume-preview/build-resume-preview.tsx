bed1cb3a4276f31b9823ac55b6825f12d0de0022
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-04-05 05:53:59 +0100

    Update build-resume-preview module and component

--- src/features/build-resume/view/build-resume-preview/build-resume-preview.tsx
+++ src/features/build-resume/view/build-resume-preview/build-resume-preview.tsx
@@ -7,6 +7,8 @@ import ZoomOut from '@labs/icons/dashboard/zoom-out.svg';
 import Settings from '@labs/icons/dashboard/settings.svg';
 
 import styles from './build-resume-preview.module.scss';
+import { TemplateModal } from './view/template-modal/template-modal';
+import { useBuildStore } from '@/store/z-store/builder';
 
 const Resume = dynamic(
 	() => import('./view/resume-blocks').then((md) => md.ResumeApp),
@@ -15,19 +17,25 @@ const Resume = dynamic(
 	}
 );
 
-export const BuildResumePreview = () => {
+export const BuildResumePreview = ({ previewRef }: { previewRef: any }) => {
+	const { template, setTemplate } = useBuildStore();
 	const [zoomLevel, setZoomLevel] = useState(1);
-	const previewRef = useRef(null);
-
+	const [showTemplateModal, setShowTemplateModal] = useState(false);
 	const handleZoomChange = (newZoom: number) => {
 		setZoomLevel(Math.min(Math.max(newZoom, 0.5), 2));
 	};
 
 	const handleZoomToFit = useCallback(() => {
+		if (!previewRef?.current) return;
 		const container = previewRef.current;
 		if (!container) return;
-		const { width } = (container as any).getBoundingClientRect();
-		const zoom = width / 842;
+		const { height } = (container as any).getBoundingClientRect();
+		const standardA4Size = { width: 595, height: 842 };
+
+		const zoom = Math.min(
+			0.73,
+			Math.min(height, standardA4Size.height) / standardA4Size.height / 1.2
+		);
 
 		handleZoomChange(zoom);
 	}, []);
@@ -51,55 +59,69 @@ export const BuildResumePreview = () => {
 	}, []);
 
 	return (
-		<div className={styles.Preview}>
-			<div ref={previewRef} style={{ position: 'relative' }}>
-				<Resume
-					scale={zoomLevel}
-					setScale={(scale: number) => {
-						setZoomLevel(scale);
-					}}
-				/>
-			</div>
-			<div className={styles.PreviewSnarkBar}>
-				<Flex
-					justifyContent="space-between"
-					className={styles.PreviewSnarkBar__inner}
-				>
-					<CallToAction.button
-						variant="secondary"
-						size="sm"
-						onClick={() => {
-							console.log('Change Template');
+		<>
+			<div className={styles.Preview}>
+				<div className="relative w-full h-full">
+					<Resume
+						scale={zoomLevel}
+						setScale={(scale: number) => {
+							setZoomLevel(scale);
 						}}
+					/>
+				</div>
+				<div className={styles.PreviewSnarkBar}>
+					<Flex
+						justifyContent="space-between"
+						className={styles.PreviewSnarkBar__inner}
 					>
-						Change Template
-					</CallToAction.button>
-
-					<Flex gap={12}>
-						<button
+						<CallToAction.button
+							variant="secondary"
+							size="sm"
 							onClick={() => {
-								handleZoomChange(zoomLevel + 0.1);
+								setShowTemplateModal(true);
 							}}
 						>
-							<ZoomIn />
-						</button>
-						<Text size="sm"> {Math.round(zoomLevel * 100)}%</Text>
-						<button
-							onClick={() => {
-								handleZoomChange(zoomLevel - 0.1);
-							}}
-						>
-							<ZoomOut />
-						</button>
-						<button onClick={handleZoomToFit}>
-							<Text size="sm">Zoom to fit</Text>
-						</button>
-						<button>
-							<Settings />
-						</button>
+							Change Template
+						</CallToAction.button>
+						<Text size="sm" weight={800}>
+							{' '}
+							{template.title}
+						</Text>
+						<Flex gap={12}>
+							<button
+								onClick={() => {
+									handleZoomChange(zoomLevel + 0.1);
+								}}
+							>
+								<ZoomIn />
+							</button>
+							<Text size="sm"> {Math.round(zoomLevel * 100)}%</Text>
+							<button
+								onClick={() => {
+									handleZoomChange(zoomLevel - 0.1);
+								}}
+							>
+								<ZoomOut />
+							</button>
+							<button onClick={() => handleZoomChange(1)}>
+								<Text size="sm">Scale</Text>
+							</button>
+							<button onClick={handleZoomToFit}>
+								<Text size="sm">Fit</Text>
+							</button>
+							<button>
+								<Settings />
+							</button>
+						</Flex>
 					</Flex>
-				</Flex>
+				</div>
 			</div>
-		</div>
+			<TemplateModal
+				template={template}
+				setTemplate={setTemplate}
+				show={showTemplateModal}
+				onClose={() => setShowTemplateModal(false)}
+			/>
+		</>
 	);
 };
