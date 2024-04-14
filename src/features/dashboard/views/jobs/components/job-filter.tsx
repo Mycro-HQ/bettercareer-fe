067bdaaba9e05d499d12b31ec5e27f4f199884bc
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-14 13:21:55 +0100

    Add job filter component to job search page

---
+++ src/features/dashboard/views/jobs/components/job-filter.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { DropdownMenu } from '@radix-ui/themes';
+import { Text, Flex, CallToAction } from '@labs/components';
+import DownIcon from '@public/images/dashboard/Down.svg';
+
+export function JobFilter() {
+	return (
+		<Flex
+			justifyContent="space-between"
+			className="items-center lg:items-start mb-4 md:mb-8 flex-col md:flex-row"
+		>
+			<div className="flex gap-x-4 md:gap-x-6 lg:gap-x-12 flex-col sm:flex-row mb-4 md:mb-0 py-2 items-center">
+				<Text
+					weight={600}
+					as="span"
+					className="cursor-pointer text-base md:text-lg"
+				>
+					All Jobs
+				</Text>
+				<Text
+					weight={600}
+					color="var(--text-gray)"
+					as="span"
+					className="cursor-pointer text-base md:text-lg"
+				>
+					Best Matches
+				</Text>
+				<Text
+					weight={600}
+					color="var(--text-gray)"
+					as="span"
+					className="cursor-pointer text-base md:text-lg"
+				>
+					Based on Resume
+				</Text>
+				<Text
+					weight={600}
+					color="var(--text-gray)"
+					as="span"
+					className="cursor-pointer text-base md:text-lg"
+				>
+					Posted this Week
+				</Text>
+				<Text
+					weight={600}
+					color="var(--text-gray)"
+					as="span"
+					className="cursor-pointer text-base md:text-lg"
+				>
+					Sponsored Jobs
+				</Text>
+			</div>
+			<JobFilterDropDown />
+		</Flex>
+	);
+}
+
+export function JobFilterDropDown() {
+	const dropDownDataArray = ['Newest', 'Oldest', 'Most Popular'];
+	const [dropDownData, setDropDownData] = React.useState(dropDownDataArray[0]);
+	const filterDropDownDataArray = dropDownDataArray.filter(
+		(data) => data !== dropDownData
+	);
+	return (
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger>
+				<CallToAction
+					outline
+					size="sm"
+					className="transition-all flex items-center gap-x-[6px] [&[data-state=open]>svg]:rotate-180"
+				>
+					<span>{dropDownData}</span>
+					<DownIcon className="transition-transform duration-200" />
+				</CallToAction>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				{filterDropDownDataArray.map((data) => (
+					<DropdownMenu.Item key={data} onClick={() => setDropDownData(data)}>
+						{data}
+					</DropdownMenu.Item>
+				))}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	);
+}
