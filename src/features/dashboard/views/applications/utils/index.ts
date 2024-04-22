d4d8d84ab537056581051a34a7df31d3c69e02da
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-22 17:48:52 +0100

    Remove handleCategoryChange function in applications/utils/index.ts

--- src/features/dashboard/views/applications/utils/index.ts
+++ src/features/dashboard/views/applications/utils/index.ts
@@ -1,24 +1,4 @@
-import React from 'react';
-
 import type { ApplicationState, ApplicationJob } from '../types';
-
-export function handleCategoryChange(
-	key: string,
-	newCategoryID: ApplicationState,
-	state: ApplicationJob[],
-	setState: React.Dispatch<React.SetStateAction<ApplicationJob[]>>
-) {
-	const job = state.find((job) => job.key === key);
-	if (job) {
-		setState((prev) => {
-			const newApplications = prev.filter(
-				(application) => application.key !== key
-			);
-			const newJob = { ...job, categoryID: newCategoryID };
-			return [...newApplications, newJob];
-		});
-	}
-}
 
 export const filterApplicationsByID = (
 	appState: ApplicationJob[],
