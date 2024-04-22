4bcd169b3c63b73dce428f471d678b1c58adfab4
Samson Okunola <smsnmicheal@gmail.com>
2024-04-22 08:36:28 +0100

    chore: added state management for preference tabs

--- src/features/dashboard/views/job-preference/preference.tsx
+++ src/features/dashboard/views/job-preference/preference.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { CallToAction, Flex } from '@labs/components';
 
@@ -6,24 +7,50 @@ import styles from './preference.module.scss';
 import { Sidebar } from './sidebar/sidebar';
 
 const preferenceList = [
-	{ label: 'Target Role', component: <div></div>, done: true },
-	{ label: 'Work Environment', component: <div></div>, done: false },
-	{ label: 'Qualifications', component: <div></div>, done: false },
-	{ label: 'Availability', component: <div></div>, done: false },
-	{ label: 'Compensation', component: <div></div>, done: false },
+	{ label: 'Target Role', component: <div>Target Role</div> },
+	{ label: 'Work Environment', component: <div>Work Environment</div> },
+	{ label: 'Qualifications', component: <div>Qualifications</div> },
+	{ label: 'Availability', component: <div>Availability</div> },
+	{ label: 'Compensation', component: <div>Compensation</div> },
 ];
 
 export const JobPreference = () => {
+	const [activeTab, setActiveTab] = React.useState(0);
+
+	const handleTabChange = (index: number) => {
+		if (index < preferenceList.length - 1) {
+			// check for valid responses before proceeding
+			setActiveTab((prev) => prev + 1);
+		} else {
+			// submit logic goes here
+			console.log('done!');
+		}
+	};
+
 	return (
 		<Flex className={styles.JobPreference}>
 			<aside className={styles.JobPreferenceAside}>
-				<Sidebar preferenceList={preferenceList} />
+				<Sidebar currentIndex={activeTab} preferenceList={preferenceList} />
 			</aside>
 			<Flex.Column className={styles.JobPreferenceContent}>
-				<Flex.Column className={styles.JobPreferenceContentBody}></Flex.Column>
-				<Flex alignItems="center" className={styles.JobPreferenceContentFooter}>
-					<CallToAction disabled>Next</CallToAction>
-				</Flex>
+				{preferenceList.map((pref, index) => (
+					<Flex.Column
+						key={index}
+						className={classNames([
+							styles.JobPreferenceContentMain,
+							styles[`JobPreferenceContentMain--${index === activeTab}`],
+						])}
+					>
+						<Flex.Column className={styles.JobPreferenceContentBody}>
+							{pref.component}
+						</Flex.Column>
+						<Flex className={styles.JobPreferenceContentFooter}>
+							<CallToAction onClick={() => handleTabChange(index)}>
+								Next
+							</CallToAction>
+						</Flex>
+					</Flex.Column>
+				))}
 			</Flex.Column>
 		</Flex>
 	);
--- src/features/dashboard/views/job-preference/sidebar/sidebar.tsx
+++ src/features/dashboard/views/job-preference/sidebar/sidebar.tsx
@@ -6,33 +6,33 @@ import { Flex, Text } from '@labs/components';
 import styles from './sidebar.module.scss';
 
 interface Props {
+	currentIndex: number;
 	preferenceList: {
 		component: React.JSX.Element;
-		done: boolean;
 		label: string;
 	}[];
 }
 
-export const Sidebar = ({ preferenceList }: Props) => {
+export const Sidebar = ({ currentIndex, preferenceList }: Props) => {
 	return (
 		<aside className={styles.Sidebar}>
 			<Flex.Column className={styles.SidebarList}>
-				{preferenceList.map((pref, i) => (
-					<Flex key={i} className={styles.SidebarListItem}>
+				{preferenceList.map((pref, index) => (
+					<Flex key={index} className={styles.SidebarListItem}>
 						<div
 							className={classNames([
 								styles.SidebarListItemIndex,
-								styles[`SidebarListItemIndex--${pref.done}`],
+								styles[`SidebarListItemIndex--${index <= currentIndex}`],
 							])}
 						>
-							<Text.span size="xs">{i + 1}</Text.span>
+							<Text.span size="xs">{index + 1}</Text.span>
 						</div>
 						<Text.p
 							size="sm"
 							weight={500}
 							className={classNames([
 								styles.SidebarListItemText,
-								styles[`SidebarListItemText--${pref.done}`],
+								styles[`SidebarListItemText--${index <= currentIndex}`],
 							])}
 						>
 							{pref.label}
