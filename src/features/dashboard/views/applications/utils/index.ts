a7daeb67eae47b16503a2eb9b7b8d6ba27fdb496
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-22 16:31:45 +0100

    Add reorderJobApplications function to applications/utils/index.ts

--- src/features/dashboard/views/applications/utils/index.ts
+++ src/features/dashboard/views/applications/utils/index.ts
@@ -24,3 +24,15 @@ export const filterApplicationsByID = (
 	appState: ApplicationJob[],
 	categoryID: ApplicationState
 ) => appState.filter((application) => application.categoryID === categoryID);
+
+export const reorderJobApplications = (
+	allJobs: ApplicationJob[],
+	job: ApplicationJob,
+	oldJobIndex: number,
+	newJobIndex: number
+) => {
+	const items = [...allJobs];
+	const [_reorderedItem] = items.splice(oldJobIndex, 1);
+	items.splice(newJobIndex, 0, job);
+	return items;
+};
