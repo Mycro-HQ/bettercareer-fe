0db83c092e52c6e3dcbd445348ebd21f34314475
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-22 18:03:01 +0100

    Remove unused function in applications/utils/index.ts

--- src/features/dashboard/views/applications/utils/index.ts
+++ src/features/dashboard/views/applications/utils/index.ts
@@ -4,15 +4,3 @@ export const filterApplicationsByID = (
 	appState: ApplicationJob[],
 	categoryID: ApplicationState
 ) => appState.filter((application) => application.categoryID === categoryID);
-
-export const reorderJobApplications = (
-	allJobs: ApplicationJob[],
-	job: ApplicationJob,
-	oldJobIndex: number,
-	newJobIndex: number
-) => {
-	const items = [...allJobs];
-	const [_reorderedItem] = items.splice(oldJobIndex, 1);
-	items.splice(newJobIndex, 0, job);
-	return items;
-};
