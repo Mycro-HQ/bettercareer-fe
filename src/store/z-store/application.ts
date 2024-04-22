966392fbf0526e25fe787def5262b924c20f8a29
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-22 17:45:54 +0100

    Add useApplicationStore to manage application state

---
+++ src/store/z-store/application.ts
@@ -0,0 +1,76 @@
+import { createReportableStore } from '../middleware/report';
+
+import type {
+	ApplicationJob,
+	ApplicationState,
+} from '@/features/dashboard/views/applications/types';
+import { applicationStateDefaultData } from '@/features/dashboard/views/applications/data';
+
+type State = {
+	applications: ApplicationJob[];
+};
+
+type Actions = {
+	addNewApplication: (newApplication: ApplicationJob) => void;
+	addNewApplicationAtIndex: (
+		newApplication: ApplicationJob,
+		index: number
+	) => void;
+	removeApplication: (applicationKey: string) => void;
+	updateApplication: (
+		applicationKey: string,
+		newApplication: ApplicationJob
+	) => void;
+	updateApplicationCategory: (
+		applicationKey: string,
+		newCategoryID: ApplicationState
+	) => void;
+	setApplications: (applications: ApplicationJob[]) => void;
+};
+
+type Store = State & Actions;
+
+const useApplicationStore = createReportableStore<Store>((set) => ({
+	applications: applicationStateDefaultData,
+	addNewApplication: (newApplication: ApplicationJob) =>
+		set((store: Store) => ({
+			...store,
+			applications: [...store.applications, newApplication],
+		})),
+	addNewApplicationAtIndex: (newApplication: ApplicationJob, index: number) =>
+		set((store: Store) => {
+			const state = [...store.applications];
+			state.splice(index, 0, newApplication);
+			return {
+				...store,
+				applications: state,
+			};
+		}),
+	removeApplication: (applicationKey: string) =>
+		set((store: Store) => ({
+			...store,
+			applications: store.applications.filter(
+				(app) => app.key !== applicationKey
+			),
+		})),
+	updateApplication: (applicationKey: string, newApplication: ApplicationJob) =>
+		set((store: Store) => ({
+			...store,
+			applications: store.applications.map((app) =>
+				app.key === applicationKey ? newApplication : app
+			),
+		})),
+	updateApplicationCategory: (
+		applicationKey: string,
+		newCategoryID: ApplicationState
+	) =>
+		set((store: Store) => ({
+			...store,
+			applications: store.applications.map((app) =>
+				app.key === applicationKey ? { ...app, categoryID: newCategoryID } : app
+			),
+		})),
+	setApplications: (applications: ApplicationJob[]) => set({ applications }),
+}));
+
+export { useApplicationStore };
