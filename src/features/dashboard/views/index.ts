6880c6aaa9e84f36e5533a8c93f4e0d97be8b899
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2024-05-16 04:18:22 +0300

    Update globals.d.ts and dashboard views, add new resume block templates and update dashboard layout and styles

--- src/features/dashboard/views/index.ts
+++ src/features/dashboard/views/index.ts
@@ -3,3 +3,4 @@ export * from './resume-analysis';
 export * from './jobs';
 export * from './applications';
 export * from './settings';
+export * from './resumes';
--- src/pages/dashboard/settings.tsx
+++ src/pages/dashboard/settings.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 import { Settings } from '@/features/dashboard/';
 import { DashboardLayout } from '@/features/dashboard/layout';
-import { useUserStore } from '@/store/z-store/user';
 
 export default function ApplicationsPage() {
-	const { profile } = useUserStore();
-
 	return (
-		<DashboardLayout title="Settings" profile={profile} plainBackdrop>
+		<DashboardLayout title="Settings" plainBackdrop>
 			<Settings />
 		</DashboardLayout>
 	);
