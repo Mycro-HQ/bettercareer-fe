24d6752ddc32c38eb725f010f6b5f618f8e8feaf
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-07-10 08:53:08 +0100

    feat: Replace waitlist with landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { Seo } from '@/components/seo';
-import { Waitlist } from '@/features/marketing/waitlist';
+import LandingPage from '@/features/marketing/landing';
 
 export const Home = () => {
 	return (
 		<>
 			<Seo title="Boost your job search with AI-resumes. Get hired!" />
-			<Waitlist />
+			<LandingPage />
 		</>
 	);
 };
