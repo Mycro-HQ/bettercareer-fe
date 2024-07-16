1718b74323a8ea6dec2caa73e6b4208e221314b1
Yasir Gaji <hellopleasantcodes@gmail.com>
2024-07-16 09:38:07 +0100

    Update: Created the transform section

---
+++ .history/src/features/marketing/landing/views/transform/data_20240716090723.tsx
@@ -0,0 +1,38 @@
+export type TransformType = {
+	key: number;
+	image: string;
+	title: string;
+	subtitle: string;
+	dataURL: string;
+	path: string;
+};
+
+export const transform: TransformType[] = [
+	{
+		key: 1,
+		image: '/images/landing/advice-1.png',
+		title: 'Upload or Build Your Resume',
+		subtitle:
+			'Quickly upload your resume or create a new one for your job applications.',
+		dataURL: 'LFIr1_%3?^.90JadVs-;8^%Nx^Rj',
+		path: '/blog/1',
+	},
+	{
+		key: 2,
+		image: '/images/landing/advice-2.png',
+		title: 'Get Matched with the Best Jobs',
+		subtitle:
+			'Discover effective techniques to confidently navigate job interviews and leave a lasting impression that sets you apart from other candidates.',
+		dataURL: 'LdJ*en~qE1xt?HM{RjWB%g-p%2IU',
+		path: '/blog/2',
+	},
+	{
+		key: 3,
+		image: '/images/landing/advice-3.png',
+		title: 'Networking 101',
+		subtitle:
+			'Explore essential networking strategies to develop meaningful professional connections and advance your career.',
+		dataURL: 'LDG+RK.T.T-o$v%$r;?v0grVW=R.',
+		path: '/blog/3',
+	},
+];
---
+++ .history/src/features/marketing/landing/views/transform/index_20240716091855.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { LandingHeading } from '../../components';
+
+import { CallToAction, Text } from '@labs/components';
+import ArrowRight from '@labs/icons/misc/arrow-right.svg';
+
+import { transform } from './data';
+import styles from './transform.module.scss';
+
+export default function Transform() {
+	return (
+		<section className="bg-white w-full align-middle py-[7.5rem]">
+			<LandingHeading
+				mainText="Transform Your Job Search Experience"
+				afterLineBreak="with Cutting-Edge "
+				highlightedText="AI Technology"
+			/>
+			<div className="flex flex-col items-center md:items-start md:flex-row flex-wrap gap-8">
+				{transform.map((a) => (
+					<div key={a.key} className="flex-1">
+						<Image
+							src={a.image}
+							alt={a.title}
+							width="391"
+							height="391"
+							placeholder="blur"
+							blurDataURL={a.dataURL}
+							className="rounded-3xl mb-9 aspect-square"
+						/>
+						<Text
+							className="font-[Recoleta]  tracking-[0.032px] mb-2"
+							color="#0F1F2E"
+							weight={600}
+							fontSize="var(--font-h5)"
+							lineHeight="29px"
+							align="center"
+						>
+							{a.title}
+						</Text>
+						<Text
+							weight={500}
+							color="#3F4C58"
+							lineHeight="23px"
+							className="tracking-[-0.192px] mb-8 sm:max-w-96"
+							align="center"
+						>
+							{a.subtitle}
+						</Text>
+						{/* <Link href={a.path} className={styles.ReadMore}>
+							Read More
+							<ArrowRight />
+						</Link> */}
+					</div>
+				))}
+			</div>
+			<CallToAction variant="primary" size="md" className="!text-[#fff] flex ">
+				Get Started Now
+			</CallToAction>
+		</section>
+	);
+}
